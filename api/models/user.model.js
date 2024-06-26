05641e1d0c3da18585036290c1edbe18e2f7a8e4
Kaan <kaanasutay10@gmail.com>
2024-06-27 02:42:30 +0300

    delete trim

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,22 +5,19 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true,
-        trim: true
+        unique: true
     },
 
     email: {
         type: String,
         required: true,
-        unique: true,
-        trim: true
+        unique: true
     },
 
     password: {
         type: String,
-        required: true,
-        trim: true
-    },
+        required: true
+    }
 
 }, { timestamps: true })
 
