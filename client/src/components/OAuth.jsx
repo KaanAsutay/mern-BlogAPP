f340d56d44987750d306e58bcf75b6107ca75ae9
Kaan <kaanasutay10@gmail.com>
2024-07-05 15:16:23 +0300

    create handleGoogleClick

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Button } from 'flowbite-react'
+import { AiFillGoogleCircle } from 'react-icons/ai'
 
 export default function OAuth() {
+
+    const handleGoogleClick = async () => {
+        
+    }
+
+
   return (
-    <div>
-      Oatuh
-    </div>
+    <Button 
+    type='button' 
+    gradientDuoTone='pinkToOrange' 
+    outline
+    onClick={handleGoogleClick}
+    >
+        <AiFillGoogleCircle className='w-6 h-6 mr-2'/>
+        Continue with Google 
+    </Button>
   )
 }
