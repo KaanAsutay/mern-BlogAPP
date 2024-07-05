7efe4665fba37e9473e0627cf166665805257127
Kaan <kaanasutay10@gmail.com>
2024-07-05 15:22:03 +0300

    add .env and firebase.js

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-blog-23a06.firebaseapp.com",
+  projectId: "mern-blog-23a06",
+  storageBucket: "mern-blog-23a06.appspot.com",
+  messagingSenderId: "660917892381",
+  appId: "1:660917892381:web:da72cf6ef9b53a64aa5df4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
