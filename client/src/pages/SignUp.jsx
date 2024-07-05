b2004a87eec7203a39a045081cfd1450a5a154bd
Kaan <kaanasutay10@gmail.com>
2024-07-05 15:10:42 +0300

    Create and connect OAuth.jsx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import OAuth from '../components/OAuth'
 
 export default function SignUp() {
   const [formData, setFormData] = useState({})
@@ -112,6 +113,7 @@ export default function SignUp() {
                   ) : 'Sign Up'
                 }
               </Button>
+              <OAuth />
 
             </form>
 
