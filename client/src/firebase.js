ba5df55317520d24706f05047526593643051c60
Kaan <kaanasutay10@gmail.com>
2024-07-05 15:30:09 +0300

    connect google authentication

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-blog-23a06.firebaseapp.com",
   projectId: "mern-blog-23a06",
   storageBucket: "mern-blog-23a06.appspot.com",
