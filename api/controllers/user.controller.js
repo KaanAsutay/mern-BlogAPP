bdb7cb41a757d223a1e01dc7ef9262879d8ca6a7
Kaan <kaanasutay10@gmail.com>
2024-07-10 02:00:21 +0300

    add deleted user API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -4,7 +4,7 @@ import User from '../models/user.model.js';
 
 export const test = (req, res) => {
     res.json({message: 'API is working!'})
-}
+};
 
 export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.userId) {
@@ -52,4 +52,16 @@ export const updateUser = async (req, res, next) => {
     } catch (error) {
       next(error);
     }
-  };
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'))
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId)
+    res.status(200).json('User has been deleted')
+  } catch (error) {
+    next(error)
+  }
+}
