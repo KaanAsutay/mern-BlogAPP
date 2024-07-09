6a136962d4b4a9f13d5691c9514b7d835449c175
Kaan <kaanasutay10@gmail.com>
2024-07-10 02:25:58 +0300

    Complete delete user account functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -41,6 +41,22 @@ const userSlice = createSlice({
         updateFailure: (state, action) => {
             state.loading = false,
             state.error = action.payload
+        },
+        
+        deleteUserStart: (state) => {
+            state.loading = true
+            state.error = null
+        },
+
+        deleteUserSuccess: (state) => {
+            state.currentUser = null
+            state.loading = false
+            state.error = null
+        },
+
+        deleteUserFailure: (state, action) => {
+            state.loading = false
+            state.error = action.payload
         }
 
     }
@@ -52,7 +68,10 @@ export const  {
             signInFailure,
             updateStart,
             updateSuccess,
-            updateFailure 
+            updateFailure,
+            deleteUserStart,
+            deleteUserSuccess,
+            deleteUserFailure 
             } = userSlice.actions
 
 export default userSlice.reducer
